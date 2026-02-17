e5f01fd93d10966e0a42d276ea336f3109cfe883
Akshaya-Vetrivel-M <akshaya.vetrivel5@gmail.com>
2026-02-17 15:34:38 +0530

    Create MultiplicationTable.java

---
+++ Loops/MultiplicationTable.java
@@ -0,0 +1,20 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        int n = 10;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " * " + i + " = " + (n * i));
+        }
+    }
+}
+
+Ouput:
+10 * 1 = 10
+10 * 2 = 20
+10 * 3 = 30
+10 * 4 = 40
+10 * 5 = 50
+10 * 6 = 60
+10 * 7 = 70
+10 * 8 = 80
+10 * 9 = 90
+10 * 10 = 100
