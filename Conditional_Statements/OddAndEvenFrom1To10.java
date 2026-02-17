89cc822241492747dcc973d7ceafa2bd7226fc5e
Akshaya-Vetrivel-M <akshaya.vetrivel5@gmail.com>
2026-02-17 19:13:07 +0530

    Create OddAndEvenFrom1To10.java

---
+++ Conditional_Statements/OddAndEvenFrom1To10.java
@@ -0,0 +1,27 @@
+public class OddAndEvenFrom1To10 {
+    public static void main(String[] args) {
+
+        System.out.println("Odd and Even numbers from 1 to 10:");
+
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " is even");
+            } else {
+                System.out.println(i + " is odd");
+            }
+        }
+    }
+}
+
+Output:
+Odd and Even numbers from 1 to 10:
+1 is odd
+2 is even
+3 is odd
+4 is even
+5 is odd
+6 is even
+7 is odd
+8 is even
+9 is odd
+10 is even
