99b874f438e432a7d96c68baa533f66e3b11e99a
Akshaya-Vetrivel-M <akshaya.vetrivel5@gmail.com>
2026-02-17 15:24:56 +0530

    Create AddingTwoNumbers.java

---
+++ Basics/AddingTwoNumbers.java
@@ -0,0 +1,10 @@
+public class AddingTwoNumbers {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+}
+
+Output:
+15
