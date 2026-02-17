279a75a6775be2a4c7690e5e440982e9fe720b7e
Akshaya-Vetrivel-M <akshaya.vetrivel5@gmail.com>
2026-02-17 15:26:32 +0530

    Create OddAndEvenNumbers.java

---
+++ Basics/OddAndEvenNumbers.java
@@ -0,0 +1,16 @@
+public class OddAndEvenNumbers {
+    public static void main(String[] args) {
+        System.out.println("Odd and even numbers from 1 to 10:");
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " is even");
+            } else {
+                System.out.println(i + " is odd");
+            }
+        }
+    }
+}
+
+Output:
+Odd and even numbers from 1 to 10:6
+6 is even
