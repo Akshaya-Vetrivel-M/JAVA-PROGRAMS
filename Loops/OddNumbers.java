673b018b1fc635900ea120daa9eb14c4906b3908
Akshaya-Vetrivel-M <akshaya.vetrivel5@gmail.com>
2026-02-17 15:31:07 +0530

    Create OddNumbers.java

---
+++ Loops/OddNumbers.java
@@ -0,0 +1,17 @@
+public class OddNumbers {
+    public static void main(String[] args) {
+        System.out.println("Odd numbers from 1 to 100 are:");
+        for (int i = 1; i <= 100; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
+
+Output:
+Odd numbers from 1 to 100 are:
+1
+3
+5
+7
+...
+99
